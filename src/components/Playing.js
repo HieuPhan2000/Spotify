be997d6d48951ad327a9ccf0a0485759c9c8228f
HieuPhan <hieuphface@gmail.com>
2022-08-08 14:08:55 +0700

    Spotify

---
+++ src/components/Playing.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { Songs } from '../Context';
+
+export default function Playing() {
+    const { song, handleSetSong } = useContext(Songs);
+    const handleClickNext = () => {
+        handleSetSong(song.id + 1);
+    };
+
+    const handleClickPre = () => {
+        handleSetSong(song.id - 1);
+    };
+
+    
+    return (
+        <div>
+            <AudioPlayer
+                className="player-music"
+                src={song.url}
+                layout="stacked-reverse"
+                autoPlay
+                showSkipControls={true}
+                showJumpControls={false}
+                onEnded={handleClickNext}
+                autoPlayAfterSrcChange={true}
+                onClickNext={handleClickNext}
+                onClickPrevious={handleClickPre}
+            />
+        </div>
+    );
+}
